815d33e670a9c26e087cd431089d409d3e766ed7
Aidan Barden <aidanbarden2005@gmail.com>
2026-03-01 15:42:51 +0000

    Ambient music and rudder/wheel rotation
    Also enabled anti-aliasing

--- Assets/Scripts/Ship/ShipMove.cs
+++ Assets/Scripts/Ship/ShipMove.cs
@@ -7,14 +7,21 @@ public class ShipMove : MonoBehaviour {
     public float maxTurnSpeed = 45f;  
     private float currentMoveSpeed = 0f;
     private float currentTurnSpeed = 0f;     
+    private float wheelSpinAngle = 0f;
     private Rigidbody Rigidbody;         
     private Vector2 moveInput;    
-    private InputSystemActions actions;       
+    private InputSystemActions actions;
+    public ParticleSystem waterExplosion;
+    public ParticleSystem waterTrail;
+    private AudioSource wind;
+    public Transform rudder;
+    public Transform wheel;
 
     private void Awake() {
         Rigidbody = GetComponent<Rigidbody>();
         actions = new InputSystemActions();
-    }
+        wind = GetComponent<AudioSource>();
+    }   
 
     private void OnEnable () {
         Rigidbody.isKinematic = false;
@@ -39,10 +46,35 @@ public class ShipMove : MonoBehaviour {
         float forward = Mathf.Max(0, moveInput.y);
         float targetSpeed = forward * maxMoveSpeed;
         currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, targetSpeed, moveAcceleration * Time.fixedDeltaTime);
+        if (currentMoveSpeed > 0.05f) {
+            if (!waterExplosion.isPlaying) {
+                //waterExplosion.Play();
+            }
+            //waterTrail.Play();
+            if(!wind.isPlaying){
+                wind.volume = 0f;
+                if (wind.clip != null) {
+                    wind.time = Random.Range(0f, wind.clip.length);
+                    wind.Play();
+                }
+            }
+            wind.volume = Mathf.MoveTowards(wind.volume, 0.8f, 0.3f * Time.deltaTime);
+        }
+        else {
+            if (waterExplosion.isPlaying) {
+                waterExplosion.Stop();
+            }
+            if (waterTrail.isPlaying) {
+                waterTrail.Stop();
+            }
+            wind.volume = Mathf.MoveTowards(wind.volume, 0f, 0.5f * Time.deltaTime);
+            if(wind.volume <= 0.01f && wind.isPlaying) {
+                wind.Stop();
+            }
+        }
         Vector3 movement = transform.forward * currentMoveSpeed * Time.fixedDeltaTime;
         Rigidbody.MovePosition(Rigidbody.position + movement);
     }
-
 
     private void Turn() {
         float targetTurnSpeed = moveInput.x * maxTurnSpeed;
@@ -50,5 +82,9 @@ public class ShipMove : MonoBehaviour {
         float turn = currentTurnSpeed * Time.fixedDeltaTime;
         Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
         Rigidbody.MoveRotation(Rigidbody.rotation * turnRotation);
+        float rudderTurn = moveInput.x * 30f;
+        rudder.localRotation = Quaternion.Euler(0f, rudderTurn, 0f);
+        wheelSpinAngle += moveInput.x * 200f * Time.fixedDeltaTime;
+        wheel.localRotation = Quaternion.Euler(0f, 0f, -wheelSpinAngle);
     }
 }
